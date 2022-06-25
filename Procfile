3f6a70cbaa1bc05883e7f9ec4229d569443ceaea
Sakib Malik <maliksakib347@gmail.com>
2022-06-25 20:22:45 +0530

    deploy herokuapp

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn w3code.wsgi 
--- w3code/settings.py
+++ w3code/settings.py
@@ -24,6 +24,7 @@ DOMAIN_NAME = '127.0.0.1'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$h8&!iox$hjs*eeo2^9gt@k!1r#4@qey-&ormv_91$lb13@-41'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -66,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'w3code.urls'
@@ -131,7 +133,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media url
 MEDIA_URL = 'media/'
